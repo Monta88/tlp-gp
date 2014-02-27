fc84918c6e5d641d30470ed726c698eb00871779
Nathan Prat <nathan.prat@gmail.com>
2014-02-27 16:57:14 +0100

    Base code of action.h

--- tlp-gp/tlp-gp/action.h
+++ tlp-gp/tlp-gp/action.h
@@ -1,8 +1,20 @@
 #pragma once
+
+#include <iostream> 
+#include <string> 
+
+using namespace std;
+
 class action
 {
 public:
 	action();
 	~action();
+private:
+	string name;
+	pair<string, string> parameters;
+	bool isDurative;
+	string preconditions; //TODO change type
+	string effect; //TODO change type
 };
 
