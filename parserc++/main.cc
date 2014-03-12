d8e8bdd91569cd90df32ed286c166c4d16dbef10
Alan <alan@Alan-PC>
2014-03-12 23:54:49 +0100

    Adding Makefile for parserc++ Modifing parserc++ main to make it parse the file given as parameter Adding the simple exemple test.pddl in parserc++ which is parsed

--- parserc++/main.cc
+++ parserc++/main.cc
@@ -1,8 +1,32 @@
 #include "parser/Parser.h"
 
+using namespace std;
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+
 int main(int argc, char **argv)
 {
     Parser parser;
+    
+	FILE * file;
+	if (argc != 2) {
+		cerr << "Usage :\n\t" << argv[0] << " PDDL_File\n";
+		exit(1);
+	}
+	
+	if ((file = fopen(argv[1], "r")) == NULL) {
+		cerr << argv[1] << " cannot be opened.\n";
+		exit(2);
+	}
+	
+	dup2(fileno(file), STDIN_FILENO);
+	
+	if (parser.parse() == 0)
+		printf("Parsing surccessful\n");
 
-    parser.parse();
+	return 0;
 }
