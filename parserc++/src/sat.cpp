3f9899a2946c726232b037e4bb496ca459ece109
nathanprat <nathan.prat@gmail.com>
2014-04-23 22:49:49 +0200

    sat: Using files to communicate with the solver

--- parserc++/src/sat.cpp
+++ parserc++/src/sat.cpp
@@ -7,18 +7,20 @@
 
 #include "sat.h"
 
-sat::sat() {
+Sat::Sat() {
 	// TODO Auto-generated constructor stub
-
-
+	FILE *m_smt2file = nullptr;
+	string solver = "";
+	ofstream m_file;
+	string m_smt2String ="";
 }
 
-sat::~sat() {
+Sat::~Sat() {
 	// TODO Auto-generated destructor stub
 	m_file.close();
 }
 
-void sat::initialize(){
+void Sat::initialize(){
 	m_file.open("~temp.smt2", ios::out );
 
 	if (m_file.is_open()){
@@ -26,13 +28,15 @@ void sat::initialize(){
 		m_file << "(declare-fun t_Init () Int)";
 		m_file << "\n(declare-fun t_Goal () Int)";
 
-		cout << m_testString << endl;
+		// TODO Add the real constraints
+		//cout << m_testString << endl;
 		m_file << m_testString ;
 
-		m_file << "(check-sat)" ;
-		m_file << "(get-value (t_Goal t_Init))" ;
+		m_smt2String += "(declare-fun t_Init () Int)";
+		m_smt2String += "(declare-fun t_Goal () Int)";
+		m_smt2String += "(set-option :produce-models true)";
+		m_smt2String += m_testString;
 
-		m_file.close();
 	}
 	else{
 		cout<< "(sat) ERROR: can not open temp file" <<endl;
@@ -40,8 +44,49 @@ void sat::initialize(){
 	}
 }
 
-bool sat::solve() {
-	//exec solver
+bool Sat::solve() {
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	// add the correct instructions to get an ouput from mathsat
+	m_file << "(check-sat)" ;
+	m_file << "(get-value (t_Goal t_Init))" ;
+	m_file.flush(); // synchronize the file
+
+	m_smt2String += "(check-sat)";
+	m_smt2String += "(get-value (t_Goal t_Init))";
+	//cout << m_smt2String << endl;
+
+	// convert iostream to a C FILE
+	m_smt2file = fopen("~temp.smt2", "r");
+	if (m_smt2file == NULL) {
+		cerr << " cannot be opened." << endl;
+		exit(2);
+	}
+
+	// create a fork because it is the easyest way to capture the output of "exec"
+	if (fork() == 0){
+	    // child
+
+		// redirect stdout to a file, and stdin from a another file
+		freopen ("~output.txt","w",stdout);
+		dup2(fileno(m_smt2file), STDIN_FILENO);
+		fclose(m_smt2file);
+
+		// execute mathsat
+		execl("./mathsat",nullptr);
+	}
+	else{
+		//parent
+		wait(null);
+	}
+
+	// close all
+	m_file.close();
+	fclose (stdout);
+
+	// read the first line of the output file which contains sat or unsat
 
 	return true;
 }
