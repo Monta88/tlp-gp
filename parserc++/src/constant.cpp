b6e5eb226f6fa6dca4defae0b24f92a02c90ab45
Alan BENIER <benier.alan@gmail.com>
2014-05-09 16:46:52 +0200

    Comments on src/constant.* done
    Modifying Constant::addTypes() in order to avoid duplicatae

--- parserc++/src/constant.cpp
+++ parserc++/src/constant.cpp
@@ -5,17 +5,34 @@ Constant::Constant(string name, vector<Type*> types): Member(name, types) {}
 Constant::Constant(): Member() {}
 
 void Constant::addTypes(vector<Type*> types) {
-	m_types.insert(m_types.end(), types.begin(), types.end());
+	// for each type if the either type in types
+	for (vector<Type*>::iterator it_types = types.begin(); it_types != types.end(); ++it_types) {
+		bool contains = false; // tell if the current type is already a type of the object
+		
+		// we verify it is not in the either type m_types of the object
+		for (vector<Type*>::iterator it_m_types = m_types.begin(); (!contains) && (it_m_types != m_types.end()); ++it_m_types) {
+			if ((*it_m_types)->getName() == (*it_types)->getName()) { // if the types are the same
+				contains = true;
+			}
+		}
+		
+		if (!contains) { // if the objects didn't have this type yet
+			// we add this type
+			m_types.push_back(*it_types);
+		}
+	}
 }
 
 string Constant::to_string() {
 	string str = "Constant " + m_name + " - ";
-	if (m_types.size() > 0) {
-		if (m_types.size() == 1) {
+	if (m_types.size() > 0) { // if it is a simple type
+		if (m_types.size() == 1) { // it has no type
 			str += m_types.at(0)->getName();
 		}
-		else {
+		else { // if it is an either type
 			str += "(either";
+			
+			// we print all the types of the either type
 			for (vector<Type*>::iterator it = m_types.begin(); it != m_types.end(); ++it) {
 				str += " " + (*it)->getName();
 			}
--- parserc++/src/constant.h
+++ parserc++/src/constant.h
@@ -1,3 +1,11 @@
+/*!
+ * \file constant.h
+ * \brief This class represent a PDDL constant, inherit from Member
+ * \author Alan BENIER, Martin LAGLEIZE, Nathan PRAT
+ * \version 1.0
+ * \date May 07th 2014
+ */
+
 #pragma once
 
 
@@ -8,12 +16,37 @@
 
 
 class Constant : public Member {
-	public:
-		Constant(string name, vector<Type*> types);
-		Constant();
-		void addTypes(vector<Type*> types);
-		string to_string();
-		string getClass(); 
+public:
+	
+	/*!
+	 * \brief Constructor
+	 */
+	Constant();
+	
+	/*!
+	 * \brief Constructor
+	 * \param name - name of the constant
+	 *        types - either type of the constant
+	 */
+	Constant(string name, vector<Type*> types);
+	
+	/*!
+	 * \brief Add an either type to the existing either type of the constant (doesn't duplicate)
+	 * \param types - either type to add to the constant
+	 */
+	void addTypes(vector<Type*> types);
+	
+	/*!
+	 * \brief Return a string that represent the constant
+	 * \return a string "Constant name - either type"
+	 */
+	string to_string();
+	
+	/*!
+	 * \brief Return the name of the class
+	 * \return "Constant"
+	 */
+	string getClass(); 
 };
 
 #endif // CONSTANT_H
