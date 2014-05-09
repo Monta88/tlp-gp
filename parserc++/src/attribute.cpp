98f2d52c8b853cf32a1130beedc742ba39b3393e
Alan BENIER <benier.alan@gmail.com>
2014-05-09 18:03:16 +0200

    Comments on src/attribute.* done

--- parserc++/src/attribute.cpp
+++ parserc++/src/attribute.cpp
@@ -1,3 +1,11 @@
+/*!
+ * \file attribute.cpp
+ * \brief This class represent a PDDL Temporally Extended attribute (invented by Frederic Maris), which are the main feature of the interval of preconditions and effects of PDDL durative-actions
+ * \author Alan BENIER, Martin LAGLEIZE, Nathan PRAT
+ * \version 1.0
+ * \date May 07th 2014
+ */
+
 #include "attribute.h"
 
 
@@ -24,18 +32,20 @@ void Attribute::addNotSupported(Interval inter) {
 	m_not_supported.push_back(inter);
 }
 
-float Attribute::getTime (){
+float Attribute::getTime() {
 	float ret = 0;
-	for(vector<Interval>::iterator it=m_supported.begin() ; it != m_supported.end() ; ++it){
-		if ((*it).getStart() == (*it).getEnd()){
-			if (  (*it).getEnd() > ret ){
-				ret =  (*it).getEnd();
+	
+	// for each interval in m_supported
+	for(vector<Interval>::iterator it = m_supported.begin(); it != m_supported.end(); ++it) {
+		if ((*it).getStart() == (*it).getEnd()) { // if the time of start and the time of end of the current interval are equals
+			if ((*it).getEnd() > ret) { // we keep the maximum time of end
+				ret = (*it).getEnd();
 			}
 		}
 	}
+	
 	return ret;
 }
-	
 
 vector<Interval> * Attribute::getForbidden() {
 	return &m_forbidden;
@@ -55,26 +65,34 @@ vector<Interval> * Attribute::getNotSupported() {
 
 string Attribute::to_string() {
 	string rep="";
-	if (m_forbidden.size() != 0) {
-		rep +="forbidden on";
+	if (m_forbidden.size() != 0) { // if there are interval that are forbidden
+		rep +=" forbidden on";
+		
+		// we print each interval that is forbidden
 		for (vector<Interval>::iterator it = m_forbidden.begin(); it != m_forbidden.end(); ++it) {
 			rep += " " + (*it).to_string();
 		}
 	}
-	if (m_supported.size() != 0) {
-		rep +="supported on";
+	if (m_supported.size() != 0) { // if there are interval that are supported
+		rep +=" supported on";
+		
+		// we print each interval that is supported
 		for (vector<Interval>::iterator it = m_supported.begin(); it != m_supported.end(); ++it) {
 			rep += " " + (*it).to_string();
 		}
 	}
-	if (m_not_forbidden.size() != 0) {
-		rep +="not forbidden on";
+	if (m_not_forbidden.size() != 0) { // if there are interval that are not forbidden
+		rep +=" not forbidden on";
+		
+		// we print each interval that is not forbidden
 		for (vector<Interval>::iterator it = m_not_forbidden.begin(); it != m_not_forbidden.end(); ++it) {
 			rep += " " + (*it).to_string();
 		}
 	}
-	if (m_not_supported.size() != 0) {
-		rep +="not supported on";
+	if (m_not_supported.size() != 0) { // if there are interval that are not supported
+		rep +=" not supported on";
+		
+		// we print each interval that is not supported
 		for (vector<Interval>::iterator it = m_not_supported.begin(); it != m_not_supported.end(); ++it) {
 			rep += " " + (*it).to_string();
 		}
--- parserc++/src/attribute.h
+++ parserc++/src/attribute.h
@@ -1,3 +1,11 @@
+/*!
+ * \file attribute.h
+ * \brief This class represent a PDDL Temporally Extended attribute (invented by Frederic Maris), which are the main feature of the interval of preconditions and effects of PDDL durative-actions
+ * \author Alan BENIER, Martin LAGLEIZE, Nathan PRAT
+ * \version 1.0
+ * \date May 07th 2014
+ */
+
 #pragma once
 
 
@@ -9,24 +17,87 @@
 #include "interval.h"
 
 class Attribute {
-	public:
-		Attribute();
-		virtual ~Attribute();
-		float getTime ();
-		void addForbidden(Interval inter);
-		void addNotForbidden(Interval inter);
-		void addSupported(Interval inter);
-		void addNotSupported(Interval inter);
-		vector<Interval> * getForbidden();
-		vector<Interval> * getSupported();
-		vector<Interval> * getNotForbidden();
-		vector<Interval> * getNotSupported();
-		string to_string();
-	private:
-		vector<Interval> m_forbidden;
-		vector<Interval> m_supported;
-		vector<Interval> m_not_forbidden;
-		vector<Interval> m_not_supported;
+public:
+	
+	/*!
+	 * \brief Constructor
+	 */
+	Attribute();
+	
+	/*!
+	 * \brief Destructor - virtual
+	 */
+	virtual ~Attribute();
+	
+	/*!
+	 * \brief Get the duration of the list of interval that are supported
+	 * \return the duration of the list of interval that are supported
+	 */
+	float getTime ();
+	
+	/*!
+	 * \brief Add an interval to the list of interval that are forbidden
+	 * \param inter - the new interval to add in m_forbidden
+	 */
+	void addForbidden(Interval inter);
+	
+	/*!
+	 * \brief Add an interval to the list of interval that are not forbidden
+	 * \param inter - the new interval to add in m_not_forbidden
+	 */
+	void addNotForbidden(Interval inter);
+	
+	/*!
+	 * \brief Add an interval to the list of interval that are supported
+	 * \param inter - the new interval to add in m_supported
+	 */
+	void addSupported(Interval inter);
+	
+	/*!
+	 * \brief Add an interval to the list of interval that are not supported
+	 * \param inter - the new interval to add in m_not_supported
+	 */
+	void addNotSupported(Interval inter);
+	
+	/*!
+	 * \brief Get the list of interval that are forbidden
+	 * \return a pointer to m_forbidden
+	 */
+	vector<Interval> * getForbidden();
+	
+	/*!
+	 * \brief Get the list of interval that are supported
+	 * \return a pointer to m_supported
+	 */
+	vector<Interval> * getSupported();
+	
+	/*!
+	 * \brief Get the list of interval that are not forbidden
+	 * \return a pointer to m_not_forbidden
+	 */
+	vector<Interval> * getNotForbidden();
+	
+	/*!
+	 * \brief Get the list of interval that are not supported
+	 * \return a pointer to m_not_supported
+	 */
+	vector<Interval> * getNotSupported();
+	
+	/*!
+	 * \brief Return a string that represent the attribute
+	 * \return a string "[forbidden on m_forbidden] [supported on m_supported] [not forbidden on m_not_forbidden] [not supported on m_not_supported]"
+	 */
+	string to_string();
+	
+private:
+	/*!< list of interval of which characteristic is forbidden */
+	vector<Interval> m_forbidden;
+	/*!< list of interval of which characteristic is supported */
+	vector<Interval> m_supported;
+	/*!< list of interval of which characteristic is not forbidden */
+	vector<Interval> m_not_forbidden;
+	/*!< list of interval of which characteristic is not supported */
+	vector<Interval> m_not_supported;
 };
 
 #endif // ATTRIBUTE_H
