5d9dc09fb6b0dbd3a06ffbed7920ad6ca5536fce
Alan BENIER <benier.alan@gmail.com>
2014-05-09 17:15:38 +0200

    Comments on src/type.* done
    Modifying Type::addParents() in order to avoid duplicatae

--- parserc++/src/type.cpp
+++ parserc++/src/type.cpp
@@ -1,3 +1,11 @@
+/*!
+ * \file type.cpp
+ * \brief This class represent a PDDL type
+ * \author Alan BENIER, Martin LAGLEIZE, Nathan PRAT
+ * \version 1.0
+ * \date May 07th 2014
+ */
+
 #include "type.h"
 #include <iostream>
 
@@ -12,7 +20,22 @@ Type::Type() {}
 Type::~Type() {}
 
 void Type::addParents(vector<Type*> parents) {
-	m_parents.insert(m_parents.end(), parents.begin(), parents.end());
+	// for each type if the either type in types
+	for (vector<Type*>::iterator it_parents = parents.begin(); it_parents != parents.end(); ++it_parents) {
+		bool contains = false; // tell if the current type is already a type of the object
+		
+		// we verify it is not in the either type m_types of the object
+		for (vector<Type*>::iterator it_m_parents = m_parents.begin(); (!contains) && (it_m_parents != m_parents.end()); ++it_m_parents) {
+			if ((*it_m_parents)->getName() == (*it_parents)->getName()) { // if the types are the same
+				contains = true;
+			}
+		}
+		
+		if (!contains) { // if the objects didn't have this type yet
+			// we add this type
+			m_parents.push_back(*it_parents);
+		}
+	}
 }
 
 string Type::getName() {
@@ -24,7 +47,9 @@ vector<Type*> * Type::getParents() {
 }
 
 bool Type::isOneOfParents(string name) {
+	// for each type of the either type of the parent
 	for (vector<Type*>::iterator it = m_parents.begin(); it != m_parents.end(); ++it) {
+		// we check if the current type is the right one, or if it is one of its parents
 		if (((*it)->getName() == name)||((*it)->isOneOfParents(name))) {
 			return true;
 		}
@@ -34,12 +59,14 @@ bool Type::isOneOfParents(string name) {
 
 string Type::to_string() {
 	string str = "Type " + m_name;
-	if (m_parents.size() > 0) {
-		if (m_parents.size() == 1) {
+	if (m_parents.size() > 0) { // if it has a type
+		if (m_parents.size() == 1) { // if it is a simple type
 			str += " (Parent " + m_parents.at(0)->getName() + ")";
 		}
-		else {
+		else { // if it is an either type
 			str += " (Parents either";
+			
+			// we print all the types of the either type
 			for (vector<Type*>::iterator it = m_parents.begin(); it != m_parents.end(); ++it) {
 				str += " " + (*it)->getName();
 			}
--- parserc++/src/type.h
+++ parserc++/src/type.h
@@ -1,3 +1,11 @@
+/*!
+ * \file type.h
+ * \brief This class represent a PDDL type
+ * \author Alan BENIER, Martin LAGLEIZE, Nathan PRAT
+ * \version 1.0
+ * \date May 07th 2014
+ */
+
 #pragma once
 
 
@@ -11,19 +19,67 @@ using namespace std;
 
 
 class Type {
-	public:
-		Type(string name, vector<Type*> parents);
-		Type(string name);
-		Type();
-		virtual ~Type();
-		void addParents(vector<Type*> parents);
-		string getName();
-		vector<Type*> * getParents();
-		bool isOneOfParents(string name);
-		string to_string();
-	private:
-		string m_name;
-		vector<Type*> m_parents;
+public:
+	
+	/*!
+	 * \brief Constructor
+	 */
+	Type();
+	
+	/*!
+	 * \brief Constructor
+	 * \param name - name of the type
+	 *        parents - either type of the parent
+	 */
+	Type(string name, vector<Type*> parents);
+	
+	/*!
+	 * \brief Constructor, the parent type is set to an empty vector
+	 * \param name - name of the object
+	 */
+	Type(string name);
+	
+	/*!
+	 * \brief Destructor - virtual
+	 */
+	virtual ~Type();
+	
+	/*!
+	 * \brief Add an either type to the existing either type of the parent type (doesn't duplicate)
+	 * \param types - either type to add to the object
+	 */
+	void addParents(vector<Type*> parents);
+	
+	/*!
+	 * \brief Get the name of the type
+	 * \return the string of the name
+	 */
+	string getName();
+	
+	/*!
+	 * \brief Get the parent of the type
+	 * \return a pointer to the either type of the parent
+	 */
+	vector<Type*> * getParents();
+	
+	/*!
+	 * \brief Check recursively if a type is one the type's parent, in m_parents
+	 * \param name - name of a the type to check in m_parents
+	 * \return true is name is one of parent's type
+	 */
+	bool isOneOfParents(string name);
+	
+	/*!
+	 * \brief Return a string that represent the type
+	 * \return a string "Type name - Parent(s) either type"
+	 */
+	string to_string();
+	
+private:
+	/*!< name of the type */
+	string m_name;
+	/*!< either type of the parent */
+	vector<Type*> m_parents;
 };
 
 #endif // TYPE_H
