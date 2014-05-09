026630ddcc824d77957d0226d745d66603e362d7
Alan BENIER <benier.alan@gmail.com>
2014-05-09 18:15:38 +0200

    Comments on src/predicate.* done

--- parserc++/src/predicate.cpp
+++ parserc++/src/predicate.cpp
@@ -1,6 +1,16 @@
+/*!
+ * \file predicate.cpp
+ * \brief This class represent a PDDL predicate
+ * \author Alan BENIER, Martin LAGLEIZE, Nathan PRAT
+ * \version 1.0
+ * \date May 07th 2014
+ */
+
 #include "predicate.h"
 
-Predicate::Predicate(string name):m_name(name){}
+Predicate::Predicate() {}
+
+Predicate::Predicate(string name):m_name(name) {}
 
 Predicate::Predicate(string name, vector< vector<Type*> > types_list):m_name(name), m_types_list(types_list) {}
 
@@ -16,14 +26,18 @@ vector< vector<Type*> > * Predicate::getTypesList() {
 
 
 string Predicate::to_string() {
-	string str = "Predicate " + m_name + " :";
+	string str = "Predicate " + m_name;
+	
+	// for each parameter
 	for(vector< vector<Type*> >::iterator it = m_types_list.begin(); it != m_types_list.end(); ++it) {
-		if ((*it).size() > 0) {
-			if ((*it).size() == 1) {
+		if ((*it).size() > 0) { // if it has a type
+			if ((*it).size() == 1) { // if it is a simple type
 				str += " " + (*it).at(0)->getName();
 			}
-			else {
+			else { // if it is an either type
 				str += " (either";
+			
+			// we print all the types of the either type
 				for (vector<Type*>::iterator it_either = (*it).begin(); it_either != (*it).end(); ++it_either) {
 					str += " " + (*it_either)->getName();
 				}
--- parserc++/src/predicate.h
+++ parserc++/src/predicate.h
@@ -1,3 +1,11 @@
+/*!
+ * \file predicate.h
+ * \brief This class represent a PDDL predicate
+ * \author Alan BENIER, Martin LAGLEIZE, Nathan PRAT
+ * \version 1.0
+ * \date May 07th 2014
+ */
+
 #pragma once
 
 
@@ -12,16 +20,54 @@ using namespace std;
 
 
 class Predicate {
-	public:
-		Predicate(string name);
-		Predicate(string name, vector< vector<Type*> > types_list);
-		virtual ~Predicate();
-		string getName();
-		vector< vector<Type*> > * getTypesList();
-		string to_string();
-	private:
-		string m_name;
-		vector< vector<Type*> > m_types_list;
+public:
+	
+	/*!
+	 * \brief Constructor
+	 */
+	Predicate();
+	
+	/*!
+	 * \brief Constructor
+	 * \param name - name of the predicate
+	 */
+	Predicate(string name);
+	
+	/*!
+	 * \brief Constructor
+	 * \param name - name of the predicate
+	 *        types_list - either type of each parameter
+	 */
+	Predicate(string name, vector< vector<Type*> > types_list);
+	
+	/*!
+	 * \brief Destructor - virtual
+	 */
+	virtual ~Predicate();
+	
+	/*!
+	 * \brief Get the name of the predicates
+	 * \return the string of its name
+	 */
+	string getName();
+	
+	/*!
+	 * \brief Get the either types of each parameters
+	 * \return a pointer to list of either types of each parameters m_types_list
+	 */
+	vector< vector<Type*> > * getTypesList();
+	
+	/*!
+	 * \brief Return a string that represent the predicate
+	 * \return a string "Predicate name - either type of each parameter"
+	 */
+	string to_string();
+	
+private:
+	/*!< name of the predicate */
+	string m_name;
+	/*!< list of either types that represent the types of each oredered parameter of the predicate */
+	vector< vector<Type*> > m_types_list;
 };
 
 #endif // PREDICATE_H
