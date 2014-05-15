8b47f35f3d6efabfdda811fc7eca2b37eac6eb7d
nathanprat <nathan.prat@gmail.com>
2014-05-15 17:42:43 +0200

    tlpgp1: [WIP]
    mostly need to complete code in generateGraph

--- parserc++/src/tlpgp1.cpp
+++ parserc++/src/tlpgp1.cpp
@@ -22,9 +22,9 @@ Tlpgp1::~Tlpgp1() {
 }
 
 void Tlpgp1::constructGraph() {
-	Graph2 graph2 = Graph2(m_domainptr, m_problemptr);
-	graph2.generateGraph();
-	m_vertex = graph2.getVertex();
+	Graph2 m_graph2 = Graph2(m_domainptr, m_problemptr);
+	m_graph2.generateGraph();
+	m_vertex = m_graph2.getVertex();
 
 	//DurativeAction *act = graph2.findAction(m_vertex,graph2.getInits(),m_problemptr->getGoals()->at(1).first);
 	//cout << act->to_stringParam() <<endl;
@@ -34,8 +34,9 @@ void Tlpgp1::constructGraph() {
 	//graph2.getVertex()->toVector();
 
 	cout << m_domainptr->getPredicates()->size() << endl;
-	vertexToActions();
+	vertexToActions(); //convert the graph to a vector of vectors
 	cout << "gggggggggggggggggg" << m_graph.size() << endl;
+
 	//cout<<	m_graph[0][0]->to_stringParam() <<endl;
 	//cout << m_graph.at(0)->to_stringParam() <<endl;
 	//cout << m_graph.at(0)[0]->to_string() <<endl;
@@ -78,7 +79,68 @@ void Tlpgp1::constructGraph() {
 		inits.push_back((*it).first);
 	}
 	cout << "vdgsdsg" << inits.size() << endl;
-	;
+
+	//Buts ← Pre(A G ) ;
+	//DONE
+
+	//TODO
+	//Pour chaque effet e de Eff(A I ) :
+		//Ajouter un intervalle I d’apparition de la proposition e à Agenda(e) ;
+	//Fin pour;*/
+
+
+	int lvl = m_graph.size() - 1;
+	cout << "ENTERING TLPGP1 CORE LOOP "<<lvl<<endl;
+	// Tant que Buts ≠ ∅
+	while(goals.size() != 0){
+		cout << "IN THE LOOP"<<endl;
+
+		//Pour chaque précondition p d’une action B, p ∈ Buts :
+		for(auto it_precond = goals.begin(); it_precond != goals.end(); ++it_precond){
+			cout << "plopp "<<endl;
+			cout << (*it_precond)->to_string()<<endl;
+
+			//Buts ← Buts – p ;
+
+			//Sélectionner (* point de backtrack *) en utilisant l'heuristique, une action A
+			//qui produit p pour B ;
+			pair<DurativeAction* , int> p;
+			p = selectAction(*it_precond,lvl,false);
+			cout << p.first->to_string() <<" "<< p.second<<endl;
+
+			//Buts ← Buts ∪ Pre(A) ;
+
+			//Poser une contrainte de précédence entre A et B ;
+
+			//Poser un intervalle I de maintien de précondition à Agenda(p) ;
+
+			//Pour chaque intervalle I’ appartenant à Agenda(¬p) :
+				//Poser une contrainte interdisant le recouvrement de I et I’.
+			//Fin pour ;
+
+			//Pour chaque effet e de A, (sauf pour p lorsque le label de p est un
+			//singleton) :
+				//Ajouter un intervalle I d’apparition de la proposition e à Agenda(e) ;
+
+				//Pour chaque intervalle I’ de Agenda(¬e) :
+					//Poser une contrainte interdisant le recouvrement de I et I’.
+				//Fin pour ;
+			//Fin pour ;
+
+
+			//Vérifier la consistance de Contraintes (appel au solveur DTP) ;
+			//En cas d’échec, retour au point de backtrack pour sélectionner une autre
+			//action A ;
+		}
+
+		goals.clear();//remove
+	}
+
+	//Si Contraintes est consistant
+	//Alors retourner le plan-solution flottant (actions sélectionnées et Contraintes)
+	//Sinon il n'y a pas de solution à ce niveau du graphe ;
+	//Fin si ;
+	//Fin. TLPGP1
 }
 
 void Tlpgp1::vertexToActions() {
@@ -108,4 +170,17 @@ void Tlpgp1::vertexToActions() {
 
 }
 
-// selectAction(vector DurativeAction*, effect)
+pair<DurativeAction* , int> Tlpgp1::selectAction(Fluent *effect, int level, bool sameLevel){
+	if(sameLevel){
+		for(auto it_act = m_graph.at(level).begin(); it_act != m_graph.at(level).end(); it_act++){
+			if(m_graph2.compareFVF2((*it_act)->getEffectsF(),effect))
+				return make_pair(*it_act,level);
+		}
+	}
+	else{
+		for(auto it_act = m_graph.at(level-1).begin(); it_act != m_graph.at(level-1).end(); it_act++){
+			if(m_graph2.compareFVF2((*it_act)->getEffectsF(),effect))
+				return make_pair(*it_act,level-1);
+		}
+	}
+}
